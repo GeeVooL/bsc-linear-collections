a6ed1c79e5cdddf251f9c375fc229ed0bbd6e511
Michał Dziewulski <michal@dziewulski.pl>
2017-11-07 10:23:09 +0100

    Implement efficiency tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <cstddef>
 #include <cstdlib>
 #include <string>
+#include <chrono>
+#include <iostream>
 
 #include "Vector.h"
 #include "LinkedList.h"
+
+using namespace std::chrono;
 
 namespace
 {
@@ -13,8 +17,32 @@ using LinearCollection = aisdi::LinkedList<T>;
 
 void perfomTest()
 {
-  LinearCollection<std::string> collection;
-  collection.append("TODO");
+  aisdi::LinkedList<std::string> list( {"Lorem", "ipsum", "dolor", "sit", "amet,", "consectetur", "adipiscing", "elit.", "Praesent", "posuere", "tortor", "quis", "iaculis", "dignissim.", "Vestibulum", "nec", "tortor", "malesuada,", "semper", "enim", "eu,", "faucibus", "lectus.", "Morbi", "urna", "risus,", "interdum", "in", "nisl", "eu,", "convallis", "tincidunt", "dolor.", "Nunc", "a", "augue", "dui.", "Nam", "ut", "dignissim", "odio.", "Quisque", "vitae", "lectus", "eget", "risus", "volutpat", "rutrum.", "Sed", "dignissim."} );
+  aisdi::Vector<std::string> vect( {"Lorem", "ipsum", "dolor", "sit", "amet,", "consectetur", "adipiscing", "elit.", "Praesent", "posuere", "tortor", "quis", "iaculis", "dignissim.", "Vestibulum", "nec", "tortor", "malesuada,", "semper", "enim", "eu,", "faucibus", "lectus.", "Morbi", "urna", "risus,", "interdum", "in", "nisl", "eu,", "convallis", "tincidunt", "dolor.", "Nunc", "a", "augue", "dui.", "Nam", "ut", "dignissim", "odio.", "Quisque", "vitae", "lectus", "eget", "risus", "volutpat", "rutrum.", "Sed", "dignissim."} );
+
+  high_resolution_clock::time_point t1 = high_resolution_clock::now();
+  list.prepend("Test");
+  high_resolution_clock::time_point t2 = high_resolution_clock::now();
+  auto duration1 = duration_cast<microseconds>( t2 - t1 ).count();
+  std::cout << "List prepend time: " << duration1 << std::endl;
+  
+  high_resolution_clock::time_point t3 = high_resolution_clock::now();
+  vect.prepend("Test");
+  high_resolution_clock::time_point t4 = high_resolution_clock::now();
+  auto duration2 = duration_cast<microseconds>( t4 - t3 ).count();
+  std::cout << "Vector prepend time: " << duration2 << std::endl;
+  
+  high_resolution_clock::time_point t5 = high_resolution_clock::now();
+  auto tmp1 = list.begin() + (list.getSize() - 1);
+  high_resolution_clock::time_point t6 = high_resolution_clock::now();
+  auto duration3 = duration_cast<microseconds>( t6 - t5 ).count();
+  std::cout << "List get last element iterator: " << duration3 << std::endl;
+  
+  high_resolution_clock::time_point t7 = high_resolution_clock::now();
+  auto tmp2 = vect.begin() + (vect.getSize() - 1);
+  high_resolution_clock::time_point t8 = high_resolution_clock::now();
+  auto duration4 = duration_cast<microseconds>( t8 - t7 ).count();
+  std::cout << "Vector get last element iterator: " << duration4 << std::endl;
 }
 
 } // namespace
